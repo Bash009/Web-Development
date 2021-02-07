5da9e5194da4dcbf1763483f3a3167ad5ef3a105
Bash009 <basbashar@gmail.com>
2021-02-07 23:14:39 +0530

    added step 3

--- jQuery/index.js
+++ jQuery/index.js
@@ -4,6 +4,17 @@
     // $("h1").css("color","red");
 // });
 
-$("h1");
+// $("h1");
 
 // To be added Game
+var gamePattern = [];
+var buttonColours = ["red","blue","green","yellow"];
+function nextSequence() {
+    var a = Math.floor(Math.random() * 4);
+    return a;
+    
+}
+var t = buttonColours();
+var randomNumber = buttonColours[t];
+gamePattern.push(randomNumber);
+
