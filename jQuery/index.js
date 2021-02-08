759606c55410173135bc8ab46d7f2f83789b107d
Bash009 <basbashar@gmail.com>
2021-02-08 19:42:31 +0530

    added parameters

--- jQuery/index.js
+++ jQuery/index.js
@@ -1,20 +1,34 @@
-// alert('Hello Me.');
+var buttonColours = ["red", "blue", "green", "yellow"];
 
-// $(document).ready(function(){
-    // $("h1").css("color","red");
-// });
-
-// $("h1");
-
-// To be added Game
 var gamePattern = [];
-var buttonColours = ["red","blue","green","yellow"];
-function nextSequence() {
-    var a = Math.floor(Math.random() * 4);
-    return a;
-    
-}
-var t = buttonColours();
-var randomNumber = buttonColours[t];
-gamePattern.push(randomNumber);
+var userClickedPattern = [];
 
+$(".btn").click(function() {
+
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+
+  //1. In the same way we played sound in nextSequence() , when a user clicks on a button, the corresponding sound should be played.
+  playSound(userChosenColour);
+
+});
+
+function nextSequence() {
+
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+
+  //4. Refactor the code in playSound() so that it will work for both playing sound in nextSequence() and when the user clicks a button.
+  playSound(randomChosenColour);
+}
+
+//2. Create a new function called playSound() that takes a single input parameter called name.
+function playSound(name) {
+
+  //3. Take the code we used to play sound in the nextSequence() function and add it to playSound().
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
